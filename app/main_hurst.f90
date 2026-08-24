d670189789d95a21f1b4ceb1299a7054736337cc
Severin Denisenko <81779013+SeverinDenisenko@users.noreply.github.com>
2026-08-24 23:57:11 +0300

    Fix unused import

--- app/main_hurst.f90
+++ app/main_hurst.f90
@@ -2,7 +2,6 @@ program main_hurst
    use io, only: read_single_column
    use hurst, only: estimate_hurst_berg, estimate_hurst_yw, estimate_hurst_rs, estimate_hurst_lssd
    use version, only: max_ver_len, ver
-   use checks, only: last_message
    use precision, only: wp
    implicit none
 
